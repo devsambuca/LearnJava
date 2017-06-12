45cf8bad851d1b7f1d29f0831433e3286f7f2ac5
Fominykh Vladimir <stoyakovi4@gmail.com>
2017-06-12 14:41:26 +0300

    Initial commit on the project

---
+++ src/main/java/net/proselyte/javase/chapter02/Example.java
@@ -0,0 +1,13 @@
+package main.java.net.proselyte.javase.chapter02;
+
+/**
+ * Простая программа на Java.
+ * Присвоить исдходному файлу имя "Example.java"
+ */
+public class Example {
+    // Эта программа нычинается с вызова метода main()
+    public static void main(String[] args) {
+        System.out.println("Простая программа на Java.");
+
+    }
+}
