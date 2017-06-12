cf86a017dcdebe6c3b5298a84ca13b6a87d780c4
Fominykh Vladimir <stoyakovi4@gmail.com>
2017-06-12 16:13:22 +0300

    Adding class Example2

---
+++ src/main/java/net/proselyte/javase/chapter02/Example2.java
@@ -0,0 +1,25 @@
+package main.java.net.proselyte.javase.chapter02;
+
+/**
+ * @author Fominykh Vladimir
+ *
+ * Это еще один короткий пример программы.
+ * Присвоить исходному файлу имя "Example2.java"
+ */
+public class Example2 {
+    public static void main(String[] args) {
+        int num; // в этой строке кода объявляется переменная с именем num
+
+        num = 100; // в этой строке кода переменная num
+                    //приясваивается значение 100
+
+        System.out.println("Это переменная num: " + num);
+
+        num = num * 2;
+
+        System.out.print("Значение переменной num * 2 равно ");
+        System.out.println(num);
+
+
+    }
+}
