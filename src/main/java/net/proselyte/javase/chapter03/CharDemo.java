9b7e185f12fcc4decebd250405d4d18509a368aa
Fominykh Vladimir <stoyakovi4@gmail.com>
2017-06-12 19:32:36 +0300

    Adding class DemoChar example with type char

---
+++ src/main/java/net/proselyte/javase/chapter03/CharDemo.java
@@ -0,0 +1,19 @@
+package main.java.net.proselyte.javase.chapter03;
+
+/**
+ * @author Fominykh Vladimir
+ *
+ * Продемонстрировать применение типа данных char
+ */
+public class CharDemo {
+    public static void main(String[] args) {
+        char ch1, ch2;
+
+        ch1 = 88; // код X
+        ch2 = 'Y';
+
+        System.out.print("ch1 и ch2: ");
+
+        System.out.println(ch1 + " " + ch2);
+    }
+}
