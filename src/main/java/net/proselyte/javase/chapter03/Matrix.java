7f3a6724a0808953012c75a664602a5270787744
Vladimir Fominykh <stoyakovi4@gmail.com>
2017-06-13 16:34:01 +0300

    Adding class Matrix initilisation two-dimensional array

--- src/main/java/net/proselyte/javase/chapter03/Matrix.java
+++ src/main/java/net/proselyte/javase/chapter03/Matrix.java
@@ -1,7 +1,24 @@
 package main.java.net.proselyte.javase.chapter03;
 
 /**
- * Created by z-FominykhVJ on 13.06.2017.
+ * @author Fominykh Vladimir
+ *
+ * Инициальзировать двухмерный массив
  */
 public class Matrix {
+    public static void main(String[] args) {
+        double m [][] = {
+                {0 * 0, 1 * 0, 2 * 0, 3 * 0},
+                {0 * 1, 1 * 1, 2 * 1, 3 * 1},
+                {0 * 2, 1 * 2, 2 * 2, 3 * 2},
+                {0 * 3, 1 * 3, 2 * 3, 3 * 3}
+        };
+        int i, j;
+
+        for (i =0; i < 4; i++) {
+            for (j= 0; j <4; j++)
+                System.out.print(m[i][j] + " ");
+            System.out.println();
+        }
+    }
 }
