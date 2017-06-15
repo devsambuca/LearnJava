5af6ca6a86683b3772a2fac2eb8d549c1d8a0e08
Vladimir Fominykh <stoyakovi4@gmail.com>
2017-06-15 16:12:20 +0300

    Adding class Nested realization of intenal loops

---
+++ src/main/java/net/proselyte/javase/chapter05/Nested.java
@@ -0,0 +1,18 @@
+package main.java.net.proselyte.javase.chapter05;
+
+/**
+ * @author Fominykh Vladimir
+ *
+ * Циклы могут быть вложенными
+ */
+public class Nested {
+    public static void main(String[] args) {
+        int i, j;
+
+        for (i = 0; i < 10; i++) {
+            for (j = i; j < 10; j++)
+                System.out.print(".");
+            System.out.println();
+        }
+    }
+}
