72b3c27857a746d28a7eae4deed33d7384283157
Vladimir Fominykh <stoyakovi4@gmail.com>
2017-06-15 11:18:11 +0300

    Adding classes Switch and StringSwitch

---
+++ src/main/java/net/proselyte/javase/chapter05/StringSwitch.java
@@ -0,0 +1,27 @@
+package main.java.net.proselyte.javase.chapter05;
+
+/**
+ * @author Fominykh Vladimir
+ *
+ * Использовать символьные строки для управления оператором switch
+ */
+public class StringSwitch {
+    public static void main(String[] args) {
+        String str = "два";
+        switch (str) {
+            case "один":
+                System.out.println("один");
+                break;
+            case "два":
+                System.out.println("два");
+                break;
+            case "три":
+                System.out.println("три");
+                break;
+            default:
+                System.out.println("не совпало");
+                break;
+
+        }
+    }
+}
