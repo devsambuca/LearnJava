e284996bff90c7cd47dfa53cfa9df14068efcd70
Fominykh Vladimir <stoyakovi4@gmail.com>
2017-06-17 11:03:39 +0300

    Refactoring class BoxDemo5

--- src/main/java/net/proselyte/javase/chapter06/BoxDemo5.java
+++ src/main/java/net/proselyte/javase/chapter06/BoxDemo5.java
@@ -4,13 +4,13 @@ package main.java.net.proselyte.javase.chapter06;
  * @author Fominykh Vladimir
  */
 
-/*
+
 public class BoxDemo5 {
     public static void main(String[] args) {
         Box mybox1 = new Box();
         Box mybox2 = new Box();
         double vol;
-
+/*
         // инициализировать каждый экземпляр класса Box
         mybox1.setDim(10, 20, 15);
         mybox2.setDim(3, 6, 9);
@@ -23,6 +23,7 @@ public class BoxDemo5 {
         System.out.println("Объем равен " + vol);
 
     }
+    */
+    }
 }
 
-*/
