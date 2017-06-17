fae3f42b6cfef43daff092456b3852f43e64a061
Fominykh Vladimir <stoyakovi4@gmail.com>
2017-06-17 10:40:04 +0300

    Adding class BoxDemo5 - uses method with parameters

--- src/main/java/net/proselyte/javase/chapter06/Box.java
+++ src/main/java/net/proselyte/javase/chapter06/Box.java
@@ -2,6 +2,8 @@ package main.java.net.proselyte.javase.chapter06;
 
 /**
  * @author Fominykh Vladimir
+ *
+ * В программе применяется метод с параметрами
  */
 public class Box {
 
@@ -12,5 +14,12 @@ public class Box {
     // вывести объем параллелепипеда
     double volume() {
         return width * height * depth;
+    }
+
+    // установить размеры параллелепипеда
+    void setDim(double w, double h, double d) {
+            width = w;
+            height = h;
+            depth = d;
     }
 }
---
+++ src/main/java/net/proselyte/javase/chapter06/BoxDemo5.java
@@ -0,0 +1,24 @@
+package main.java.net.proselyte.javase.chapter06;
+
+/**
+ * @author Fominykh Vladimir
+ */
+public class BoxDemo5 {
+    public static void main(String[] args) {
+        Box mybox1 = new Box();
+        Box mybox2 = new Box();
+        double vol;
+
+        // инициализировать каждый экземпляр класса Box
+        mybox1.setDim(10, 20, 15);
+        mybox2.setDim(3, 6, 9);
+        //получить объем первого параллепипеда
+        vol = mybox1.volume();
+        System.out.println("Объем равен " + vol);
+
+        // получить объем второго параллелепида
+        vol = mybox2.volume();
+        System.out.println("Объем равен " + vol);
+
+    }
+}
