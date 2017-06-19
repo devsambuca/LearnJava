8ebf4d7d47961599a2ad831212b903db313d44ce
Fominykh Vladimir <stoyakovi4@gmail.com>
2017-06-19 22:59:56 +0300

    Adding classes Stack and TestStack

---
+++ src/main/java/net/proselyte/javase/chapter06/Stack.java
@@ -0,0 +1,35 @@
+package main.java.net.proselyte.javase.chapter06;
+
+/**
+ * @author Fominykh Vladimir
+ *
+ * В этом классе определяется целочисленный стек, в котором
+ * можно хранить до 10 целочисленных значений
+ */
+public class Stack {
+    int stck[] = new int[10];
+    int tos;
+
+    //инициализировать вершину стека
+    Stack() {
+        tos = -1;
+    }
+
+    //разместить элемент в стеке
+    void push (int item) {
+        if (tos == 9) System.out.println("Стек заполнен.");
+        else
+            stck[++tos] = item;
+    }
+
+    // извлечь элемент из стека
+    int pop() {
+        if (tos < 0) {
+            System.out.println("Стек не загружен.");
+            return 0;
+        }
+        else
+            return stck[tos--];
+
+    }
+}
