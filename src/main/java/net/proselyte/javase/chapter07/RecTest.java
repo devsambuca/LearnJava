ab69892919f2c1185a8c67e6144f535898786bc6
Vladimir Fominykh <stoyakovi4@gmail.com>
2017-06-20 15:03:05 +0300

    Adding class Recursion2 and RecTest other example of recursion

---
+++ src/main/java/net/proselyte/javase/chapter07/RecTest.java
@@ -0,0 +1,21 @@
+package main.java.net.proselyte.javase.chapter07;
+
+/**
+ * @author Fominykh Vladimir
+ *
+ * Еще один пример рекурсии
+ */
+
+public class RecTest {
+    int values[];
+    RecTest(int i) {
+        values = new int[i];
+    }
+
+    // вывести рекурсивно элементы массива
+    void printArray(int i) {
+        if (i == 0) return;
+        else printArray(i - 1);
+        System.out.println("[" + (i -1) + " ] " + values[i - 1]); ;
+    }
+}
