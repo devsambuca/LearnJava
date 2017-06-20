fcedf49fdeb09878b376c885c458b7d5bad9607d
Fominykh Vladimir <stoyakovi4@gmail.com>
2017-06-21 01:10:01 +0300

    Adding classes VarArgs4

---
+++ src/main/java/net/proselyte/javase/chapter07/VarArgs3.java
@@ -0,0 +1,41 @@
+package main.java.net.proselyte.javase.chapter07;
+
+/**
+ * @author Fominykh Vladimir
+ *
+ * Аргументы переменной длины и перегрузка
+ */
+public class VarArgs3 {
+
+    static void vaTest(int ... v) {
+        System.out.print("vaTest(int ...): " + "Количество аргументов: " + v.length + " Содержимое: ");
+
+        for(int x : v)
+            System.out.print(x + " ");
+        System.out.println();
+    }
+
+    static void vaTest(boolean ... v) {
+        System.out.print("vaTest(boolean ...): " + "Количество аргументов: " + v.length + " Содержимое: ");
+
+        for (boolean x : v)
+            System.out.print(x + " ");
+        System.out.println();
+    }
+
+    static void vaTest(String msg, int ... v) {
+     System.out.print("vaTest(String, int ...): " +  "Количество аргументов: " + v.length + " Содержимое: ");
+
+        for(int x : v)
+            System.out.print(x + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        vaTest(1, 2, 3);
+        vaTest("Проверка: ", 10, 20);
+        vaTest(true, false, false);
+    }
+
+
+}
---
+++ src/main/java/net/proselyte/javase/chapter07/VarArgs4.java
@@ -0,0 +1,27 @@
+package main.java.net.proselyte.javase.chapter07;
+
+/**
+ * @author Fominykh Vladimir
+ *
+ * Аргументы перемвенной длины, перегрузка и неоднозначность
+ *
+ * Эта программа содержит ошибку, и поэтому не может быть скомпилирована!
+ */
+public class VarArgs4 {
+
+    static void vaTest(int ... v) {
+        System.out.print("vaTest(int ...): " + "Количество аргументов: " + v.length + " Содержимое: ");
+
+        for(int x : v)
+            System.out.print(x + " ");
+        System.out.println();
+    }
+
+    static void vaTest(boolean ... v) {
+        System.out.print("vaTest(boolean ...): " + "Количество аргументов: " + v.length + " Содержимое: ");
+
+        for (boolean x : v)
+            System.out.print(x + " ");
+        System.out.println();
+    }
+}
