aec11d05004b568fb80004dc586d2d76a83378c2
Fominykh Vladimir <stoyakovi4@gmail.com>
2017-06-25 21:06:04 +0300

    Adding class ColorBox

---
+++ src/main/java/net/proselyte/javase/chapter08/ColorBox.java
@@ -0,0 +1,15 @@
+package main.java.net.proselyte.javase.chapter08;
+
+/**
+ * @author Fominykh Vladimir
+ */
+public class ColorBox extends Box {
+    int color; //цвет праллелепипеда
+
+    ColorBox (double w, double h, double d, int c) {
+        width = w;
+        height = h;
+        depth = d;
+        color = c;
+    }
+}
