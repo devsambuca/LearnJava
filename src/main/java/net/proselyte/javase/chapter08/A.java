3a732207dd7ba6ffc486b6b9fca066dc0b3a89cb
Fominykh Vladimir <stoyakovi4@gmail.com>
2017-06-25 19:57:59 +0300

    Adding class A and B

---
+++ src/main/java/net/proselyte/javase/chapter08/A.java
@@ -0,0 +1,12 @@
+package main.java.net.proselyte.javase.chapter08;
+
+/**
+ * @author Fominykh Vladimir
+ */
+public class A {
+    int i, j;
+
+    void showij() {
+        System.out.println("i и j: " + i + "." + j);
+    }
+}
---
+++ src/main/java/net/proselyte/javase/chapter08/B.java
@@ -0,0 +1,15 @@
+package main.java.net.proselyte.javase.chapter08;
+
+/**
+ * @author Fominykh Vladimir
+ */
+public class B extends A {
+    int k;
+
+    void showk() {
+        System.out.println("k: " + k);
+    }
+    void sum() {
+        System.out.println("i + j + k: " +(i + j + k));
+    }
+}
