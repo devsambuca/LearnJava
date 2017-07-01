466ab22f2ad5614ae39033437ec647a132334945
Fominykh Vladimir <stoyakovi4@gmail.com>
2017-07-01 23:52:10 +0300

    Adding interface IntStack

---
+++ src/main/java/net/proselyte/javase/chapter09/IntStack.java
@@ -0,0 +1,9 @@
+package main.java.net.proselyte.javase.chapter09;
+
+/**
+ * @author Fominykh Vladimir
+ */
+public interface IntStack {
+    void push(int item);    // сохранить элемент в стеке
+    int pop();              // извлечь элемент из стека
+}
