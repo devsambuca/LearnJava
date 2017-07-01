e264b2df93f334ccc978052ffc42e5c2c0b3fef0
Fominykh Vladimir <stoyakovi4@gmail.com>
2017-07-02 01:07:09 +0300

    Adding class IFTest3

---
+++ src/main/java/net/proselyte/javase/chapter09/IFTest3.java
@@ -0,0 +1,32 @@
+package main.java.net.proselyte.javase.chapter09;
+
+/**
+ * @author Fominykh Vladimir
+ */
+
+// Создать переменную интерфейса и обратиться к стекам через нее
+public class IFTest3 {
+    public static void main(String[] args) {
+        IntStack mystack; // создать переменную ссылки на интерфейс
+        DynStack ds = new DynStack(5);
+        FixedStack fs = new FixedStack(8);
+
+        mystack = ds; //загрузить динамический стек
+        // разместить числа в стеке
+        for (int i = 0; i < 12; i++) mystack.push(i);
+
+        mystack = fs; //загрузить фиксированный стек
+        // разместить числа в стеке
+        for (int i = 0; i < 8; i++) mystack.push(i);
+
+        mystack = ds;
+        System.out.println("Значения в динамическом стеке:");
+        for (int i = 0; i < 12; i++)
+            System.out.println(mystack.pop());
+
+        mystack = fs;
+        System.out.println("Значения в фиксированном стеке:");
+        for (int i = 0; i < 8; i++)
+            System.out.println(mystack.pop());
+    }
+}
