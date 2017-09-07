5220922ae698f80a9b146bc7aeabba55a2eebfe1
Vladimir Fominykh <stoyakovi4@gmail.com>
2017-09-07 16:06:59 +0300

    Adding class FileInputStreamDemo

--- src/main/java/net/proselyte/javase/chapter20/DirListOnly.java
+++ src/main/java/net/proselyte/javase/chapter20/DirListOnly.java
@@ -12,7 +12,7 @@ public class DirListOnly {
     public static void main(String[] args) {
         String dirname = "/java";
         File f1 = new File(dirname);
-        FilenameFilter only = new OnlyExt("html");
+        FilenameFilter only = new OnlyExt("class");
         String s[] = f1.list(only);
 
         for (int i = 0; i < s.length; i++) {
