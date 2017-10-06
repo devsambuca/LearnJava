167888aff9174ac448b737dd16710ca33a76fc07
Vladimir Fominykh <stoyakovi4@gmail.com>
2017-10-06 16:05:17 +0300

    Adding class Developer

---
+++ src/main/java/net/proselyte/javase/chapter21/Developer/Developer.java
@@ -0,0 +1,57 @@
+package main.java.net.proselyte.javase.chapter21.Developer;
+
+/**
+ * @author Fominykh Vladimir
+ */
+public class Developer {
+
+    private long id;
+    private String name;
+    private String surname;
+    private String job;
+    private double total;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+
+
+}
