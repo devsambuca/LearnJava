950dfee9b9803e91a14c39b7423a49020ea151a2
Fominykh <stoyakovi4@gmail.com>
2017-11-21 13:32:21 +0300

    Added classes SqrtTransform and ForkJoinDemo

---
+++ src/main/java/net/proselyte/javase/chapter28/SqrtTransform.java
@@ -0,0 +1,54 @@
+package main.java.net.proselyte.javase.chapter28;
+
+import java.util.concurrent.RecursiveAction;
+
+/**
+ * Простой пример реализации стратегии "разделяй и властвуй".
+ * В данном примере применяется класс RecursiveAction
+ *
+ * Created by smb on 21.11.2017.
+ *
+ * Класс ForkJoinTask преобразует (через класс RecursiveAction)
+ * значения элементов массива типа double в их квадратные корни
+ */
+public class SqrtTransform extends RecursiveAction {
+    // В данном примере пороговое значение произвольно устанавливается
+    // равным 1000. В реальном коде его оптимальное значение может
+    // быть определено в результате профилирования исполняющей системы
+    // или экспериментально
+    final int seqThreshold = 1000;
+
+    // обрабатываемый массив
+    double[] data;
+
+    // определить часть обрабатываемых данных
+    int start, end;
+
+    SqrtTransform(double[] vals, int s, int e) {
+        data = vals;
+        start = s;
+        end = e;
+    }
+    // Этот метод выполняет параллельное вычисление
+    protected void compute() {
+        // Если количество элементов меньше порогового значения,
+        // выполнить дальнейшую обработку последовательно
+        if ((end - start) < seqThreshold) {
+            // преобразовать значение каждого элемента массива
+            // в его квадратный корень
+            for (int i = start; i < end; i++) {
+                data[i] = Math.sqrt(data[i]);
+            }
+        } else {
+            // в противном случае продолжить разделение данных на
+            // меньшие части
+
+            // найти середину
+            int middle = (start + end) / 2;
+
+            // запустить новые подзадачи на выполнение, используя
+            // разделенные на части данные
+            invokeAll(new SqrtTransform(data, start, middle), new SqrtTransform(data, middle, end));
+        }
+    }
+}
